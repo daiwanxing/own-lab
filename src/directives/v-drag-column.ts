25e3cc8a9c808e9f1bedd4ab08af92641bd0d7a5
daiwanxing <377099119@qq.com>
2022-03-06 20:52:34 +0800

    feat: 拖拽指令

--- src/directives/v-drag-column.ts
+++ src/directives/v-drag-column.ts
@@ -1,10 +1,82 @@
 import type { Directive, DirectiveBinding } from "vue";
+import style from "./v-drag-column.module.scss";
+import { isObject } from "lodash-es";
 
-const vDragColumn: Directive<HTMLElement, DirectiveBinding> = {
-    mounted (element, binding: DirectiveBinding) {
-        // binding.value; 
+type DragProps = {
+    // 目标容器最小可调整的宽度， 默认是容器节点的计算后的样式宽度
+    minWidth: number;
+    // 目标容器最大可调整的宽度， 默认值是表示最大调整的阈值是DOM节点的父宽度
+    // 注意，如果目标容器有兄弟节点，如果调整到最大宽度为父级宽度，可能会影响其他兄弟节点布局样式
+    // 而且不一定会成功设置成父级等宽，具体实际宽度受css布局影响。
+    maxWidth: number; 
 
-        // 1. element 表示指令绑定的节点，如果是组件，就是该组件的根节点，如果组件有多根，则指令被忽略
-        // 2. 将节点的
+}
+export const vDragColumn: Directive<HTMLElement> = {
+    mounted(el, binding: DirectiveBinding<DragProps>) {
+        const props = isObject(binding.value) ? binding.value : {  } as DragProps;
+        const positionPropertyWhiteList = ["relative", "absolute", "sticky", "fixed"];
+        if (!positionPropertyWhiteList.includes(window.getComputedStyle(el, null).position)) el.style.position = "relative";
+
+        const colResizeNode = document.createElement("div");
+        colResizeNode.classList.add(style["col-resize"]);
+        el.append(colResizeNode);
+
+        const colResizeConfigOption = {
+            modifyWidth: 0,
+            minWidth: 0,
+            colResizeOffsetLeft: 0,
+            colResizeWidth: 0,
+        };
+
+        const { clientWidth } = el;
+
+        Object.assign(colResizeConfigOption, {
+            minWidth: props.minWidth || clientWidth,
+            maxWidth: props.maxWidth || el.parentElement?.clientWidth || clientWidth,
+            modifyWidth: clientWidth,
+        });
+
+        colResizeConfigOption.minWidth = props.minWidth || clientWidth;
+        colResizeConfigOption.modifyWidth = clientWidth;
+        colResizeNode.addEventListener("mousedown", enableDocumentMouseEnterHandle);
+
+        function enableDocumentMouseEnterHandle(this: typeof colResizeNode) {
+            genrateLineHandle.call(this);
+            const { left } = this.getBoundingClientRect();
+            colResizeConfigOption.colResizeOffsetLeft = left;
+            document.documentElement.addEventListener("mousemove", moveLine);
+            document.documentElement.addEventListener("mouseup", removeLineHandle);
+        }
+
+        let ruleNode: HTMLElement;
+        function genrateLineHandle(this: typeof colResizeNode) {
+            ruleNode = document.createElement("div");
+            ruleNode.classList.add(style["rule-line"]);
+            this.append(ruleNode);
+        }
+
+        function moveLine(evt: Event) {
+            const mouseEvt = evt as MouseEvent;
+            const pageX = mouseEvt.pageX;
+            const { modifyWidth: clientWidth, colResizeOffsetLeft } = colResizeConfigOption;
+
+            if (colResizeConfigOption.minWidth < clientWidth + pageX - colResizeOffsetLeft) {
+                colResizeConfigOption.colResizeWidth = pageX - colResizeOffsetLeft;
+                ruleNode.style.transform = `translateX(${colResizeConfigOption.colResizeWidth}px)`;
+            }
+        }
+
+        function removeLineHandle() {
+            if (ruleNode) {
+                const { colResizeWidth, modifyWidth } = colResizeConfigOption;
+                el.style.width = modifyWidth + colResizeWidth + "px";
+                colResizeConfigOption.modifyWidth = modifyWidth + colResizeWidth;
+                ruleNode.remove();
+            }
+
+            colResizeConfigOption.colResizeWidth = 0;
+            document.documentElement.removeEventListener("mousemove", moveLine);
+            document.documentElement.removeEventListener("mouseup", removeLineHandle);
+        }
     },
 };
--- src/root-app.vue
+++ src/root-app.vue
@@ -2,12 +2,9 @@
   <main>
     <div
       ref="containerNode"
+      v-drag-column
       class="box"
     >
-      <div
-        ref="colResizeNode"
-        class="col-resize"
-      ></div>
     </div>
     <div class="another"></div>
   </main>
@@ -16,101 +13,22 @@
 <script lang="ts">
 import "./index.scss";
 import type { VNode, RendererElement } from "vue";
-import { onMounted, shallowRef, onUnmounted, createVNode, defineComponent, render } from "vue";
+import { onMounted , shallowRef, onUnmounted, createVNode, defineComponent, render } from "vue";
+import { vDragColumn } from "@/directives/v-drag-column";
 
 export default defineComponent({
   name: "RootApp",
+  directives: {
+    dragColumn: vDragColumn
+  },
   setup() {
-    const colResizeNode = shallowRef<HTMLElement>();
-    const containerNode = shallowRef<HTMLElement>();
-    let lineVnode: RendererElement | VNode;
-
-    const colResizeConfigOption: {
-      minWidth: number;
-      modifyWidth: number;
-      colResizeOffsetLeft: number;
-      colResizeWidth: number;
-    } = {
-      minWidth: 0, // 要拖拽宽度的目标容器最初的宽度,
-      modifyWidth: 0, // 目标容器拖拽后的宽度
-      colResizeOffsetLeft: 0, // 要拖拽的线条距离视口左侧的距离
-      colResizeWidth: 0, // 拖拽的线条实际偏移了的宽度（从拖拽前 到 拖拽完毕后中间这段距离的宽）
-    };
-
-
-    function genrateLineHandle(this: HTMLElement) {
-      lineVnode = createVNode("div", { class: "line" });
-      render(lineVnode as VNode, this);
-    }
-
-    function removeLineHandle(this: HTMLElement) {
-      const el = lineVnode.el as HTMLElement;
-      if (el && containerNode.value) {
-        const { colResizeWidth, modifyWidth } = colResizeConfigOption;
-        containerNode.value.style.width = modifyWidth + colResizeWidth + "px";
-        colResizeConfigOption.modifyWidth = modifyWidth + colResizeWidth;
-      }
-
-      colResizeConfigOption.colResizeWidth = 0;
-      if (lineVnode) render(null, colResizeNode.value!);
-      document.documentElement.removeEventListener("mousemove", moveLine);
-      document.documentElement.removeEventListener("mouseup", removeLineHandle);
-    }
-
-    // 移动那根线
-    function moveLine(evt: Event) {
-      const mouseEvt = evt as MouseEvent;
-      const pageX = mouseEvt.pageX;
-      const { modifyWidth: clientWidth, colResizeOffsetLeft } = colResizeConfigOption;
-      const el = lineVnode.el as HTMLElement;
-      // 拖拽不能改变容器的最小宽度 （最小宽度默认是容器挂载到DOM的初始宽度）
-      if (colResizeConfigOption.minWidth < clientWidth + pageX - colResizeOffsetLeft) {
-        colResizeConfigOption.colResizeWidth = pageX - colResizeOffsetLeft;
-        el.style.transform = `translateX(${colResizeConfigOption.colResizeWidth}px)`;
-      }
-    }
-
-    function enableDocumentMouseEnterHandle(this: HTMLElement) {
-      genrateLineHandle.call(this);
-      const { left } = this.getBoundingClientRect();
-      colResizeConfigOption.colResizeOffsetLeft = left;
-      document.documentElement.addEventListener("mousemove", moveLine);
-      document.documentElement.addEventListener("mouseup", removeLineHandle);
-    }
-
-    onMounted(() => {
-      const colResizeElement = colResizeNode.value;
-      const containerElement = containerNode.value;
-      if (colResizeElement && containerElement) {
-        const { clientWidth } = containerElement;
-        colResizeConfigOption.minWidth = clientWidth;
-        colResizeConfigOption.modifyWidth = clientWidth;
-        colResizeElement.addEventListener("mousedown", enableDocumentMouseEnterHandle);
-      }
-    });
-
-    onUnmounted(() => {
-      if (colResizeNode.value) colResizeNode.value.removeEventListener("mousedown", enableDocumentMouseEnterHandle);
-    });
-
-    return {
-      colResizeNode,
-      containerNode,
-    };
+    //
   }
 });
 
 </script>
 
 <style lang="scss">
-.line {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  border: 2px solid orange;
-}
-
 body {
   padding: 10px 30px;
 }
@@ -132,18 +50,6 @@ main {
   height: 300px;
   border: 2px solid orange;
   transition: all 0.25s;
-  // background-color: #2970ff;
-
-  .col-resize {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    right: 0;
-    width: 5px;
-    z-index: 1;
-    // background-color: #2970ff;
-    cursor: col-resize;
-  }
 }
 </style>
 
