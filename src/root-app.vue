2381c4644a1ad84bd790173c62e1347b85d4c67c
daiwanxing <wanxing.dai@coollu.com.cn>
2022-07-14 15:22:36 +0800

    chore: 调整依赖

--- src/root-app.vue
+++ src/root-app.vue
@@ -1,25 +1,10 @@
 <template>
   <div>
     <h1>dsahdasdasijodsa</h1>
-    <Cl-page />
-    <VerifyCode />
-    <createVnode />
   </div>
 </template>
 
 <script lang="ts" setup>
-import ClPage from "@/component/page.vue";
-import VerifyCode from "@/component/verfy-code.vue";
 import "./index.scss";
-import createVnode from "@/component/createVnode.vue";
 
 </script>
-
-<style lang="scss">
-@use "./styles/var.scss";
-@use "./styles/base.scss";
-
-.example {
-  color: orange;
-}
-</style>
